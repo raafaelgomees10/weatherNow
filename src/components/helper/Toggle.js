6da3daad7ffb3e6ff8227345f4003a45667a5905
Rafael Gomes <raafaelgomees10@gmail.com>
2024-04-23 19:19:29 -0300

    WIP

--- src/components/helper/Toggle.js
+++ src/components/helper/Toggle.js
@@ -6,16 +6,16 @@ import Moon from '../../assets/moon.png';
 import Sun from '../../assets/sun.png';
 
 export const ThemeToggle = ({ theme, setTheme }) => {
-  const [isChecked, setIsChecked] = useState(
+  const [isDarkChecked, setIsDarkChecked] = useState(
     localStorage.getItem('isDarkMode') === 'true'
   );
 
   useEffect(() => {
-    setIsChecked(localStorage.getItem('isDarkMode') === 'true');
+    setIsDarkChecked(localStorage.getItem('isDarkMode') === 'true');
   }, [theme]);
 
   const handleToggleChange = () => {
-    const newChecked = !isChecked;
+    const newChecked = !isDarkChecked;
     localStorage.setItem('isDarkMode', newChecked);
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
@@ -26,13 +26,17 @@ export const ThemeToggle = ({ theme, setTheme }) => {
         data-testid="button-theme-toggle"
         type="checkbox"
         id="mode-toggle"
-        checked={isChecked}
+        checked={isDarkChecked}
         onChange={handleToggleChange}
       />
       <Container>
-        <Light $isChecked={isChecked}></Light>
-        <Dark $isChecked={isChecked}></Dark>
-        <Ball htmlFor="mode-toggle" $isChecked={isChecked}></Ball>
+        <Light data-testid="light-background" $isDarkChecked={isDarkChecked} />
+        <Dark data-testid="dark-background" $isDarkChecked={isDarkChecked} />
+        <Ball
+          data-testid="mode-toggle"
+          htmlFor="mode-toggle"
+          $isDarkChecked={isDarkChecked}
+        />
       </Container>
     </div>
   );
@@ -47,7 +51,6 @@ const Container = styled.div`
   padding-top: 5px;
   margin-top: -5px;
 `;
-
 const Background = styled.div`
   position: absolute;
   width: 100%;
@@ -58,12 +61,12 @@ const Background = styled.div`
 
 const Light = styled(Background)`
   background-image: url(${LightBg});
-  opacity: ${({ $isChecked }) => ($isChecked ? '0' : '1')};
+  opacity: ${({ $isDarkChecked }) => ($isDarkChecked ? '0' : '1')};
 `;
 
 const Dark = styled(Background)`
   background-image: url(${DarkBg});
-  opacity: ${({ $isChecked }) => ($isChecked ? '1' : '0')};
+  opacity: ${({ $isDarkChecked }) => ($isDarkChecked ? '1' : '0')};
 `;
 
 const Ball = styled.label`
@@ -77,8 +80,9 @@ const Ball = styled.label`
   cursor: pointer;
   transition: transform 0.5s;
   background-size: contain;
-  background-image: url(${({ $isChecked }) => ($isChecked ? Moon : Sun)});
-  transform: translateX(${({ $isChecked }) => ($isChecked ? '60px' : 'unset')});
+  background-image: url(${({ $isDarkChecked }) => $isDarkChecked ? Moon : Sun});
+  transform: translateX(${({ $isDarkChecked }) => ($isDarkChecked ? '60px' : 'unset')}
+  );
 `;
 
 const ModeToggle = styled.input`
--- src/components/helper/toggle.unit.spec.js
+++ src/components/helper/toggle.unit.spec.js
@@ -1,27 +1,72 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { ThemeToggle } from './Toggle';
 import userEvent from '@testing-library/user-event';
+
+const setTheme = jest.fn();
 
 describe('Toggle', () => {
   beforeEach(() => {
     localStorage.clear();
   });
 
-  it('Should render ThemeToggle and key isDarkMode must be null', () => {
-    const setTheme = jest.fn();
+  it('Should render ThemeToggle', () => {
     render(<ThemeToggle theme="light" setTheme={setTheme} />);
     expect(screen.getByTestId('theme-toggle')).toBeInTheDocument();
-    expect(localStorage.getItem('isDarkMode')).toBeNull();
   });
 
-  it('Should change isDarkMode key in localStorage when toggle is clicked', async () => {
-    const setTheme = jest.fn();
+  it('Should call setTheme when toggle is clicked', async () => {
     render(<ThemeToggle theme="light" setTheme={setTheme} />);
 
     const button = screen.getByTestId('button-theme-toggle');
 
     await userEvent.click(button);
 
-    expect(localStorage.getItem('isDarkMode')).toBe('true');
+    expect(setTheme).toHaveBeenCalledTimes(1);
+    expect(setTheme).toHaveBeenCalledWith(expect.any(Function));
   });
+
+  it('Should toggle theme every time is clicked', async () => {
+    render(<ThemeToggle theme="light" setTheme={setTheme} />);
+
+    const button = screen.getByTestId('button-theme-toggle');
+    let setThemeCallback;
+
+    await userEvent.click(button);
+    setThemeCallback = setTheme.mock.calls[0][0];
+    expect(setThemeCallback('light')).toBe('dark');
+
+    await userEvent.click(button);
+    setThemeCallback = setTheme.mock.calls[1][0]; // A chamada anterior está em mock.calls[0]
+    expect(setThemeCallback('dark')).toBe('light');
+
+    await userEvent.click(button);
+    setThemeCallback = setTheme.mock.calls[2][0]; //terceira chamada
+    expect(setThemeCallback('light')).toBe('dark');
+  });
+
+  it('Should render ThemeToggle with css propierts to LIGHT mode', () => {
+    render(<ThemeToggle theme="light" setTheme={setTheme} />);
+
+    const lightBackground = screen.getByTestId('light-background');
+    const darkBackground = screen.getByTestId('dark-background');
+    const ball = screen.getByTestId('mode-toggle');
+
+    expect(lightBackground).toHaveStyle('opacity: 1');
+    expect(darkBackground).toHaveStyle('opacity: 0');
+    expect(ball).toHaveStyle('transform: translateX(unset)');
+  });
+
+  // fit('Should render ThemeToggle with css propierts to DARK mode', async () => {
+  //   render(<ThemeToggle theme="dark" setTheme={setTheme} />);
+  //   const button = screen.getByTestId('button-theme-toggle');
+  //   await userEvent.click(button);
+
+  //   const lightBackground = screen.getByTestId('light-background');
+  //   const darkBackground = screen.getByTestId('dark-background');
+  //   const ball = screen.getByTestId('mode-toggle');
+
+  //   await waitFor(() => {
+  //     expect(lightBackground).toHaveStyle('opacity: 0');
+  //   });
+  // });
 });
